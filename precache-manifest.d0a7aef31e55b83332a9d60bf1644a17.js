d03d2ad154122373ae07563c05e778e3cdaeb2dd
sanyaslovo <sanyaslovo@gmail.com>
2020-10-18 22:32:23 +0300

    Updates

---
+++ precache-manifest.d0a7aef31e55b83332a9d60bf1644a17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20895a0747951e56d689e51a64a2d74",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "46400b445dc1f590c88d",
+    "url": "/React-ToDo-App/static/css/main.0a851504.chunk.css"
+  },
+  {
+    "revision": "70138668391e3c35e2ad",
+    "url": "/React-ToDo-App/static/js/2.65029c19.chunk.js"
+  },
+  {
+    "revision": "46400b445dc1f590c88d",
+    "url": "/React-ToDo-App/static/js/main.4d82f9b0.chunk.js"
+  },
+  {
+    "revision": "0f100f75415b839dd0a4",
+    "url": "/React-ToDo-App/static/js/runtime-main.6759c931.js"
+  },
+  {
+    "revision": "04bce1bad57c42f2363ab204267bb4eb",
+    "url": "/React-ToDo-App/static/media/add.04bce1ba.svg"
+  },
+  {
+    "revision": "10c581b6e3a0bc8ce7ac0527a423c2ad",
+    "url": "/React-ToDo-App/static/media/close.10c581b6.svg"
+  },
+  {
+    "revision": "e6d3868a10f056f22a226b8cbe602bc9",
+    "url": "/React-ToDo-App/static/media/edit.e6d3868a.svg"
+  },
+  {
+    "revision": "95f30ee9e97d62b2666b1f9c48e00ae7",
+    "url": "/React-ToDo-App/static/media/remove.95f30ee9.svg"
+  }
+]);
